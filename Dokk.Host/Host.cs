2b9b067cfb2c3edd2a26cbc6eb51a2faa73dfa7f
Dominik Hauerstein <dcdominoes@gmail.com>
2020-11-02 04:18:15 +0000

    Add project files.

---
+++ Dokk.Host/Host.cs
@@ -0,0 +1,22 @@
+﻿using Dokk.Shared;
+using System;
+using System.Windows.Forms;
+
+namespace Dokk.Host
+{
+    public partial class Host : Form
+    {
+        public Host()
+        {
+            InitializeComponent();
+        }
+
+        private DBConnection connection = new DBConnection();
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (connection.createHostSession())
+                textBox1.Text = connection.sessionCode;
+        }
+    }
+}
---
+++ Dokk.Shared/DBConnection.cs
@@ -0,0 +1,65 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Dokk.Shared
+{
+    public class DBConnection
+    {
+        private MySqlConnectionStringBuilder dbbuild = new MySqlConnectionStringBuilder();
+        private MySqlConnection dbconn = new MySqlConnection();
+        private string query;
+        private MySqlCommand dbcomm = new MySqlCommand();
+        private MySqlDataReader dbread;
+
+        public string sessionCode;
+
+        public DBConnection()
+        {
+            dbbuild.Server = "remotemysql.com";
+            dbbuild.UserID = Encoding.ASCII.GetString(new byte[] { 114, 78, 76, 68, 120, 81, 76, 102, 85, 67 });
+            dbbuild.Password = Encoding.ASCII.GetString(new byte[] { 101, 107, 87, 113, 75, 80, 86, 111, 82, 65 });
+            dbbuild.Database = Encoding.ASCII.GetString(new byte[] { 114, 78, 76, 68, 120, 81, 76, 102, 85, 67 });
+
+            dbconn.ConnectionString = dbbuild.ToString();
+            dbconn.Open();
+        }
+
+        public bool createHostSession()
+        {
+            try
+            {
+                sessionCode = "";
+                for (int i = 0; i < 6; i++)
+                {
+                    sessionCode += new Random().Next(i, 9).ToString();
+                }
+
+                uploadCode(sessionCode);
+
+                return true;
+            }
+            catch { return false; }
+        }
+
+        private void uploadCode(string code)
+        {
+            try
+            {
+                dbcomm.Parameters.Clear();
+                dbcomm = dbconn.CreateCommand();
+                dbcomm.CommandText = "INSERT INTO `Dokk` (`Code`, `UsedBy`, `StatusCode`, `lastUsed`) VALUES (@c,@u,@s,@l)";
+                dbcomm.Parameters.AddWithValue("@c", code);
+                dbcomm.Parameters.AddWithValue("@u", "null");
+                dbcomm.Parameters.AddWithValue("@s", 0);
+                dbcomm.Parameters.AddWithValue("@l", DateTime.Now.ToString("yyyy-MM-dd hh:mm:ss"));
+                dbcomm.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
